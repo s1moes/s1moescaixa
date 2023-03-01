3baad75f6599e42b43ca7d35fe89b0e6ee2de5ef
Rute <Rute@DESKTOP-PTV3IA1>
2023-03-01 14:37:03 +0000

    First Commit

---
+++ src/caixa/Main.java
@@ -0,0 +1,14 @@
+package caixa;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("******");
+		System.out.println("*    *");
+		System.out.println("*    *");
+		System.out.println("*    *");
+		System.out.println("******");
+
+	}
+
+}
