2b3fd8bde30165465802a46b75d8a6563547f7a3
Rute <Rute@DESKTOP-PTV3IA1>
2023-03-01 14:41:45 +0000

    mudarTamanho

--- src/caixa/Main.java
+++ src/caixa/Main.java
@@ -3,12 +3,14 @@ package caixa;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("******");
+		printTOpOrBottom();
 		System.out.println("*    *");
 		System.out.println("*    *");
 		System.out.println("*    *");
-		System.out.println("******");
+		printTOpOrBottom();
 
 	}
-
+	public static void printTOpOrBottom() {
+		System.out.println("******");
+	}
 }
