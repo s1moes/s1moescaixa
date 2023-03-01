a499420148e73d3fa95d26e3cb465b0272c7f79c
Rute <Rute@DESKTOP-PTV3IA1>
2023-03-01 14:46:18 +0000

    Parameters

--- src/caixa/Main.java
+++ src/caixa/Main.java
@@ -3,14 +3,17 @@ package caixa;
 public class Main {
 
 	public static void main(String[] args) {
-		printTOpOrBottom();
+		printTOpOrBottom(4);
 		System.out.println("*    *");
 		System.out.println("*    *");
 		System.out.println("*    *");
-		printTOpOrBottom();
+		printTOpOrBottom(4);
 
 	}
-	public static void printTOpOrBottom() {
-		System.out.println("******");
+	public static void printTOpOrBottom(int size) {
+		for (int i = 0; i <size ; i++) {
+		System.out.println("*");	
+		}
+		System.out.println();
 	}
 }
