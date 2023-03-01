3768c2a6d9b3c2363f886d9281f1643fd1c65f6d
Rute <Rute@DESKTOP-PTV3IA1>
2023-03-01 14:53:42 +0000

    Criar print TopOrBottom e print Left anda right

--- src/caixa/Main.java
+++ src/caixa/Main.java
@@ -3,17 +3,24 @@ package caixa;
 public class Main {
 
 	public static void main(String[] args) {
-		printTOpOrBottom(4);
-		System.out.println("*    *");
-		System.out.println("*    *");
-		System.out.println("*    *");
-		printTOpOrBottom(4);
+		printTOpOrBottom(6);
+		printLeftandRight(6, 2);
+		printTOpOrBottom(6);
 
 	}
 	public static void printTOpOrBottom(int size) {
 		for (int i = 0; i <size ; i++) {
-		System.out.println("*");	
+		System.out.print("*");	
 		}
 		System.out.println();
+	}
+	public static void printLeftandRight(int width, int height) {
+		for (int i = 0; i < height; i++) {
+			System.out.print("*");
+			for (int j = 0; j < width - 2; j++) {
+				System.out.print(" ");
+			}
+			System.out.println("*");
+		}
 	}
 }
